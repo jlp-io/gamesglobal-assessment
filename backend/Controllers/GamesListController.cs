c30bdf5ddb237c4430c31728b57ac312457a1837
jamie paterson <jamiepaterson@plan.com>
2024-05-20 08:33:20 +0200

    feat: add CRUD endpoints, chevron studio sliders, games grid css

--- backend/Controllers/GamesListController.cs
+++ backend/Controllers/GamesListController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 using Npgsql;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace backend.Controllers;
 
@@ -9,59 +10,89 @@ namespace backend.Controllers;
 [Route("[controller]")]
 public class GamesListController : ControllerBase
 {
-    // private static readonly string[] Summaries = new[]
-    // {
-    //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    // };
     private readonly AppDbContext _context;
-    // private readonly ILogger<GamesListController> _logger;
-    // public GamesListController(ILogger<GamesListController> logger)
-    // {
-    //     _logger = logger;
-    // } 
+
     public GamesListController(AppDbContext context)
     {
         _context = context;
     } 
 
-        // GET: api/GamesList
-        [HttpGet(Name = "GetGamesList")]
-        public async Task<ActionResult<IEnumerable<GamesList>>> GetGamesList()
-        {
-            return await _context.gamess.ToListAsync();
-        }
+    // CRUD endpoints
 
-        // GET: api/GamesList/{studio_name}
-        // [HttpGet("{studio_name}")]
-        // public async Task<ActionResult<GamesList>> GetGamesListByStudioName(string studio_name)
-        // {
-        //     var gamesByStudio = await _context.gamess.FindAsync(studio_name);
-
-        //     if (gamesByStudio == null)
-        //     {
-        //         return NotFound();
-        //     }
-
-        //     return gamesByStudio;
-        // }
-
-    [HttpPost(Name = "PostGamesList")]
-    public IEnumerable<GamesList> Set()
+    // GET: api/GamesList
+    [HttpGet(Name = "GetGamesList")]
+    public async Task<ActionResult<IEnumerable<GamesList>>> GetGamesList()
     {
+        return await _context.gamess.ToListAsync();
+    }
 
-        return Enumerable.Range(1, 5).Select(index => new GamesList
+    //GET: api/GamesList/{studio_name}
+    [HttpGet("{studio_name}")]
+    public async Task<ActionResult<IEnumerable<GamesList>>> GetGamesListByStudioName(string studio_name)
+    {
+        var gamesByStudio = await _context.gamess.Where(x => x.studio_name == studio_name).ToListAsync();
+        if (gamesByStudio == null)
         {
-            game_title = "",
-            genre = "",  
-            platform = "",
-            release_date = "",
-            description = "",
-            studio_name = "",
-            image_url = "",
-            rating = 0,
-            price = 0
-        })
-        .ToArray();
+            return NotFound();
+        }
+        return gamesByStudio;
+    }
+
+    //POST: api/GamesList
+    [HttpPost(Name = "PostGamesList")]
+    public async Task<ActionResult<IEnumerable<GamesList>>> PostGamesList(GamesList game)
+    {
+    _context.gamess.Add(game);
+    await _context.SaveChangesAsync();
+    //    return CreatedAtAction("PostTodoItem", new { id = todoItem.Id }, todoItem);
+    return CreatedAtAction(nameof(PostGamesList), new { id = game.id }, game);
+    }
+
+    //PUT: api/GamesList
+    [HttpPut(Name = "PutGamesList")]
+    public async Task<ActionResult<IEnumerable<GamesList>>> PutGamesList(int id, GamesList game)
+    {
+    if (id != game.id)
+    {
+        return BadRequest();
+    }
+
+    _context.Entry(game).State = EntityState.Modified;
+    try
+    {
+        await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        if (!GamesListExists(id))
+        {
+            return NotFound();
+        }
+        else
+        {
+            throw;
+        }
+    }
+    return NoContent();
+    }
+
+    private bool GamesListExists(int id)
+    {
+        return _context.gamess.Any(e => e.id == id);
+    }
+
+    //DELETE: api/GamesListDelete
+    [HttpDelete(Name = "DeleteGamesList")]
+    public async Task<ActionResult<IEnumerable<GamesList>>> DeleteGamesList(int id)
+    {
+    var gamesList = await _context.gamess.FindAsync(id);
+    if (gamesList == null)
+    {
+        return NotFound();
+    }
+    _context.gamess.Remove(gamesList);
+    await _context.SaveChangesAsync();
+    return NoContent();
     }
 
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
-import StudioRibbon from "./StudioRibbon.js";
-import GameCard from "./GameCard.js";
 import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-const MAX_GAME_VIEW = 10;
 const API_URL = "https://localhost:7126/gameslist";
 
-function GameCardContainer(props) {
-  props.games.map((g) => {
-    return <p>{g.studio_name}</p>;
-  });
-}
-
 function App() {
+  const [studioLabelData, setStudioLabelData] = useState(null);
   const [data, setData] = useState(null);
   const [show, setShow] = useState(false);
   const [selectedGame, setSelectedGame] = useState(null);
+  const [selectedStudio, setSelectedStudio] = useState("All Studios");
+  const [minGameView, setMinGameView] = useState(0);
+  const [maxGameView, setMaxGameView] = useState(10);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((json) => setStudioLabelData(json))
+      .catch((error) => console.error(error));
+  }, []);
 
   useEffect(() => {
     fetch(API_URL)
@@ -29,60 +31,124 @@ function App() {
       .catch((error) => console.error(error));
   }, []);
 
-  const studios = data
-    ? data.map((d) => {
+  const studios = studioLabelData
+    ? studioLabelData.map((d) => {
         return d.studio_name;
       })
     : [];
 
+  const filterGamesByStudio = (studioName) => {
+    console.log(studioName);
+    fetch("" + API_URL + "/" + studioName)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <StudioRibbon studios={studios} />
-
-        <div style={{ height: "200px" }}></div>
-
+        {/* {data && <StudioRibbon studios={studios} />} */}
+        {studioLabelData && (
+          <div style={{ backgroundColor: "gray" }}>
+            {minGameView > 0 && (
+              <a
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                }}
+                onClick={() => {
+                  setMinGameView(minGameView - 10);
+                  setMaxGameView(maxGameView - 10);
+                }}
+              >
+                {"<"}
+              </a>
+            )}
+            {maxGameView < 11 && (
+              <a
+                style={{
+                  color: selectedStudio === "All Studios" ? "green" : "white",
+                  textDecoration: "none",
+                }}
+                href=""
+              >
+                All Studios
+              </a>
+            )}
+            {studios.slice(minGameView, maxGameView).map((studio_name) => {
+              return (
+                <a
+                  style={{
+                    color: selectedStudio === studio_name ? "green" : "white",
+                    textDecoration: "none",
+                  }}
+                  onClick={() => {
+                    filterGamesByStudio(studio_name);
+                    setSelectedStudio(studio_name);
+                  }}
+                >
+                  {" " + studio_name + " "}
+                </a>
+              );
+            })}
+            <a
+              style={{
+                color: "white",
+                textDecoration: "none",
+              }}
+              onClick={() => {
+                setMinGameView(minGameView + 10);
+                setMaxGameView(maxGameView + 10);
+              }}
+            >
+              {">"}
+            </a>
+          </div>
+        )}
+        <div style={{ height: "150px" }}></div>
         {data ? (
           <div style={{ width: "1000px" }}>
-            {data.slice(0, 10).map((d, item) => {
+            {data.slice(0, 12).map((d, item) => {
               return (
                 <img
+                  style={{ border: "4px solid #a9a9a9" }}
                   width="200"
                   height="200"
                   src={data[item].image_url}
                   onClick={() => {
                     setSelectedGame(data[item]);
                     handleShow();
+                    console.log(studioLabelData);
                   }}
                 ></img>
               );
             })}
-
-            {selectedGame && (
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Body>
-                  <tr>
-                    <td>Game Title: {selectedGame.game_title}</td>
-                    <td>Description: {selectedGame.description}</td>
-                    <td>Genre: {selectedGame.genre}</td>
-                    <td>Platform: {selectedGame.platform}</td>
-                    <td>Release Date: {selectedGame.release_date}</td>
-                    <td>Rating: {selectedGame.rating}</td>
-                    <td>Price: {selectedGame.price}</td>
-                  </tr>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            )}
           </div>
         ) : (
           "Loading..."
         )}
       </header>
+      {selectedGame && (
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Body>
+            <tr>
+              <td>Game Title: {selectedGame.game_title}</td>
+              <td>Description: {selectedGame.description}</td>
+              <td>Genre: {selectedGame.genre}</td>
+              <td>Platform: {selectedGame.platform}</td>
+              <td>Release Date: {selectedGame.release_date}</td>
+              <td>Rating: {selectedGame.rating}</td>
+              <td>Price: {selectedGame.price}</td>
+            </tr>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
   );
 }
